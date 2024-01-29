7ef77d9872ee2fbea21b0bcfe670b656c56fd425
Jacob Jung1~ <jacobj@live.ca>
2024-01-29 00:36:44 -0500

    Adds image below after uploading

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,45 @@
+"use client"
+import React, { useState } from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Home() {
-  
-  async function handleOnSubmit(e: React.SyntheticEvent) {
-  }
-  
+  const [imageSrc, setImageSrc] = useState<string | null>(null); // Allow imageSrc to be string or null
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files ? e.target.files[0] : null;
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (readerEvent) => {
+        // Assure TypeScript that the result is a string
+        const result = readerEvent.target?.result;
+        if (typeof result === 'string') {
+          setImageSrc(result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-
-  
-
-
-    <div>
-      <input type="file" name="image"/>
-    </div>
+    <main>
+      <div className="flex h-full w-full">
+        <Card>
+          <CardHeader>
+            <CardTitle>Pixel Palette</CardTitle>
+            <CardDescription>Upload A File</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" onChange={handleFileChange} />
+            {imageSrc && <img src={imageSrc} alt="Uploaded" style={{ maxWidth: '100%', height: 'auto' }} />}
+          </CardContent>
+        </Card>
+      </div>
+    </main>
   );
 }
