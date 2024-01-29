fa4656ccc17aa498944ca1d86faa0b4e30ca2b48
Jacob Jung1~ <jacobj@live.ca>
2024-01-29 00:05:47 -0500

    handleOnSubmit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 
 export default function Home() {
+  
+  async function handleOnSubmit(e: React.SyntheticEvent) {
+  }
+  
   return (
 
+  
 
 
-    
     <div>
       <input type="file" name="image"/>
     </div>
